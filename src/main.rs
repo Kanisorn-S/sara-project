b0dd4e1d51659cd57b7742b1a9bbbb1b784cbadb
Kanisorn-S <karamahati@gmail.com>
2025-08-30 22:09:48 +0700

    feat: Inital commit

---
+++ src/main.rs
@@ -0,0 +1,32 @@
+#![no_main]
+#![no_std]
+
+use cortex_m_rt::entry;
+use stm32h7xx_hal::{pac, prelude::*};
+use panic_halt as _;
+
+#[entry]
+fn main() -> ! {
+    let cp = cortex_m::Peripherals::take().unwrap();
+    let dp = pac::Peripherals::take().unwrap();
+
+    let pwr = dp.PWR.constrain();
+    let pwrcfg = pwr.freeze();
+
+    let rcc = dp.RCC.constrain();
+    let ccdr = rcc.sys_ck(100.MHz()).freeze(pwrcfg, &dp.SYSCFG);
+
+    let gpioe = dp.GPIOE.split(ccdr.peripheral.GPIOE);
+
+    let mut led = gpioe.pe3.into_push_pull_output();
+
+    let mut delay = cp.SYST.delay(ccdr.clocks);
+
+    loop {
+        led.set_high();
+        delay.delay_ms(500_u16);
+
+        led.set_low();
+        delay.delay_ms(500_u16);
+    }
+}
