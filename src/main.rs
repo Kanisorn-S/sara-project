91c2cc5105cef7b3000f45244f35289515f0cec1
Kanisorn-S <karamahati@gmail.com>
2025-09-04 14:25:01 +0700

    feat: implement blinky on hardware level

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,43 @@
-#![no_main]
 #![no_std]
+#![no_main]
 
+use core::ptr::{read_volatile, write_volatile};
+use cortex_m::asm::nop;
 use cortex_m_rt::entry;
-use stm32h7xx_hal::{pac, prelude::*};
+use stm32h7xx_hal as _;
 use panic_halt as _;
 
 #[entry]
 fn main() -> ! {
-    let cp = cortex_m::Peripherals::take().unwrap();
-    let dp = pac::Peripherals::take().unwrap();
+    const RCC_AHB4ENR_ADDR: *mut u32 = 0x5802_44E0 as *mut u32;
+    const GPIOE_MODER_ADDR: *mut u32 = 0x5802_1000 as *mut u32;
+    const GPIOE_ODR_ADDR: *mut u32 = 0x5802_1014 as *mut u32;
 
-    let pwr = dp.PWR.constrain();
-    let pwrcfg = pwr.freeze();
+    unsafe {
+        const RCC_AHB4ENR_GPIOEEN: u32 = 1 << 4;
+        let rcc_val = read_volatile(RCC_AHB4ENR_ADDR);
+        write_volatile(RCC_AHB4ENR_ADDR, rcc_val | RCC_AHB4ENR_GPIOEEN);
 
-    let rcc = dp.RCC.constrain();
-    let ccdr = rcc.sys_ck(100.MHz()).freeze(pwrcfg, &dp.SYSCFG);
+        let current_moder = read_volatile(GPIOE_MODER_ADDR);
 
-    let gpioe = dp.GPIOE.split(ccdr.peripheral.GPIOE);
+        let cleared_moder = current_moder & !(0b11 << 6);
 
-    let mut led = gpioe.pe3.into_push_pull_output();
+        let output_moder = cleared_moder | (0b01 << 6);
 
-    let mut delay = cp.SYST.delay(ccdr.clocks);
+        write_volatile(GPIOE_MODER_ADDR, output_moder);
+    }
+
+    const TOGGLE_GPIOE_PIN_3: u32 = 1 << 3;
 
     loop {
-        led.set_high();
-        delay.delay_ms(500_u16);
+        unsafe {
+            let current_gpioe = read_volatile(GPIOE_ODR_ADDR);
+            let toggle_pin_3 = current_gpioe ^ TOGGLE_GPIOE_PIN_3;
+            write_volatile(GPIOE_ODR_ADDR, toggle_pin_3);
+        }
 
-        led.set_low();
-        delay.delay_ms(500_u16);
+        for _ in 0..400_000 {
+            nop();
+        }
     }
 }
