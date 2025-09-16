187b482d7f962662ca1d188d92e7cd8097531bd0
Kanisorn-S <karamahati@gmail.com>
2025-09-16 09:04:47 +0700

    feat: complete adc implementation

--- src/adc/mod.rs
+++ src/adc/mod.rs
@@ -1,29 +1,55 @@
 use core::ptr::{read_volatile, write_volatile};
+use crate::gpio::{gpio_set_pin, GpioPort};
 use crate::timer::delay;
+
+const LED_PIN_PORT: GpioPort = GpioPort::E;
+const LED_PIN: u8 = 3;
 
 const RCC_BASE: u32 = 0x5802_4400;
 const RCC_AHB1ENR_ADDR: *mut u32 = (RCC_BASE + 0x0D8) as *mut u32;
+const RCC_D3CCIPR_ADDR: *mut u32 = (RCC_BASE + 0x058) as *mut u32;
 
 const ADC1_BASE: u32 = 0x4002_2000;
 const ADC1_CR_ADDR: *mut u32 = (ADC1_BASE + 0x008) as *mut u32;
 const ADC1_ISR_ADDR: *mut u32 = (ADC1_BASE + 0x000) as *mut u32;
 const ADC1_SQR1_ADDR: *mut u32 = (ADC1_BASE + 0x030) as *mut u32;
 const ADC1_DR_ADDR: *const u32 = (ADC1_BASE + 0x040) as *const u32;
+const ADC1_PCSEL: *mut u32 = (ADC1_BASE + 0x01C) as *mut u32;
+
 
 pub unsafe fn rcc_enable_adc_clock() {
     const RCC_AHB1ENR_ADC12EN: u32 = 1 << 5;
-    let rcc_val = read_volatile(RCC_AHB1ENR_ADDR);
-    write_volatile(RCC_AHB1ENR_ADDR, rcc_val | RCC_AHB1ENR_ADC12EN);
+    let rcc_ahb1enr = read_volatile(RCC_AHB1ENR_ADDR);
+    write_volatile(RCC_AHB1ENR_ADDR, rcc_ahb1enr | RCC_AHB1ENR_ADC12EN);
+
+    let mut rcc_d3ccipr = read_volatile(RCC_D3CCIPR_ADDR);
+    rcc_d3ccipr &= !(0b11 << 16);
+    rcc_d3ccipr |= 1 << 17;
+    write_volatile(RCC_D3CCIPR_ADDR, rcc_d3ccipr);
 }
 
 pub unsafe fn adc_power_up() {
-    write_volatile(ADC1_CR_ADDR, 1 << 28); // ADVREGEN = 1
+    // Exit Deep Power Down mode
+    const ADC1_CR_DEEPPWD: u32 = 1 << 29;
+    let adc1_cr = read_volatile(ADC1_CR_ADDR);
+    write_volatile(ADC1_CR_ADDR, adc1_cr & !(ADC1_CR_DEEPPWD));
+
+    // Enable ADC Voltage Regulator
+    const ADC1_CR_ADVREGEN: u32 = 1 << 28;
+    let adc1_cr = read_volatile(ADC1_CR_ADDR);
+    write_volatile(ADC1_CR_ADDR,  adc1_cr | ADC1_CR_ADVREGEN);
     delay(20_000); // Wait for regulator startup
 }
 
 pub unsafe fn adc_calibrate() {
-    // Start calibration with ADVREGEN still enabled
-    write_volatile(ADC1_CR_ADDR, (1 << 31) | (1 << 28)); // ADCAL = 1
+    const ADC1_CR_ADCALDIF: u32 = 1 << 30;
+    let adc1_cr = read_volatile(ADC1_CR_ADDR);
+    write_volatile(ADC1_CR_ADDR, adc1_cr & !(ADC1_CR_ADCALDIF));
+    
+    const ADC1_CR_ADCAL: u32 = 1 << 31;
+    let adc1_cr = read_volatile(ADC1_CR_ADDR);
+    write_volatile(ADC1_CR_ADDR, adc1_cr | ADC1_CR_ADCAL);
+
     // Wait until calibration is finished (ADCAL bit goes to 0)
     while (read_volatile(ADC1_CR_ADDR) & (1 << 31)) != 0 {}
 }
@@ -38,8 +64,15 @@ pub unsafe fn adc_enable() {
 }
 
 pub unsafe fn adc_configure_single_conversion(channel: u32) {
-    // Set sequence length to 1 (L=0) and first conversion to the desired channel
-    write_volatile(ADC1_SQR1_ADDR, channel << 6);
+    let ADC1_SQR1_CHANNEL: u32 = channel << 6;
+    let adc1_sqr1 = read_volatile(ADC1_SQR1_ADDR);
+    write_volatile(ADC1_SQR1_ADDR, adc1_sqr1 | ADC1_SQR1_CHANNEL);
+}
+
+pub unsafe fn adc_preselect_channel(channel: u32) {
+    const ADC1_PCSEL_16: u32 = 1 << 16;
+    let adc1_pcsel = read_volatile(ADC1_PCSEL);
+    write_volatile(ADC1_PCSEL, adc1_pcsel | ADC1_PCSEL_16);
 }
 
 pub unsafe fn adc_start_conversion() {
--- src/main.rs
+++ src/main.rs
@@ -9,17 +9,22 @@ use cortex_m::asm::nop;
 use cortex_m_rt::entry;
 use stm32h7xx_hal as _;
 use panic_halt as _;
-use crate::adc::{adc_calibrate, adc_configure_single_conversion, adc_enable, adc_power_up, adc_read_value, adc_start_conversion, adc_wait_for_conversion, rcc_enable_adc_clock};
-use crate::gpio::{gpio_set_pin_mode, gpio_toggle_pin, rcc_enable_gpio_clock, GpioPort, PinMode};
+use crate::adc::{adc_calibrate, adc_configure_single_conversion, adc_enable, adc_power_up, adc_preselect_channel, adc_read_value, adc_start_conversion, adc_wait_for_conversion, rcc_enable_adc_clock};
+use crate::gpio::{gpio_set_pin, gpio_set_pin_mode, gpio_toggle_pin, rcc_enable_gpio_clock, GpioPort, PinMode};
 use crate::timer::delay;
 
 #[entry]
 fn main() -> ! {
+    const LED_PIN_PORT: GpioPort = GpioPort::E;
+    const LED_PIN: u8 = 3;
     const ADC_PIN_PORT: GpioPort = GpioPort::A;
     const ADC_PIN: u8 = 0;
-    const ADC_CHANNEL: u32 = 0;
+    const ADC_CHANNEL: u32 = 16;
     
     unsafe {
+        rcc_enable_gpio_clock(LED_PIN_PORT);
+        gpio_set_pin_mode(LED_PIN_PORT, LED_PIN, PinMode::OUTPUT);
+
         rcc_enable_gpio_clock(ADC_PIN_PORT);
         rcc_enable_adc_clock();
         
@@ -29,16 +34,20 @@ fn main() -> ! {
         adc_calibrate();
         adc_enable();
         adc_configure_single_conversion(ADC_CHANNEL);
+        adc_preselect_channel(ADC_CHANNEL);
     }
     
     loop {
         let adc_value: u32;
         unsafe {
+            gpio_toggle_pin(LED_PIN_PORT, LED_PIN);
+            delay(1_000_000);
             adc_start_conversion();
             adc_wait_for_conversion();
+            gpio_toggle_pin(LED_PIN_PORT, LED_PIN);
             adc_value = adc_read_value();
         }
-        
+
         nop();
         delay(100_000);
     }
