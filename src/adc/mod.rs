c7e0afccf1877b8adbfb4600d5ece8bfbde8134c
Kanisorn-S <karamahati@gmail.com>
2025-09-16 13:52:16 +0700

    feat: complete adc implementation

--- src/adc/mod.rs
+++ src/adc/mod.rs
@@ -1,9 +1,5 @@
 use core::ptr::{read_volatile, write_volatile};
-use crate::gpio::{gpio_set_pin, GpioPort};
 use crate::timer::delay;
-
-const LED_PIN_PORT: GpioPort = GpioPort::E;
-const LED_PIN: u8 = 3;
 
 const RCC_BASE: u32 = 0x5802_4400;
 const RCC_AHB1ENR_ADDR: *mut u32 = (RCC_BASE + 0x0D8) as *mut u32;
@@ -15,6 +11,28 @@ const ADC1_ISR_ADDR: *mut u32 = (ADC1_BASE + 0x000) as *mut u32;
 const ADC1_SQR1_ADDR: *mut u32 = (ADC1_BASE + 0x030) as *mut u32;
 const ADC1_DR_ADDR: *const u32 = (ADC1_BASE + 0x040) as *const u32;
 const ADC1_PCSEL: *mut u32 = (ADC1_BASE + 0x01C) as *mut u32;
+const ADC1_DIFSEL: *mut u32 = (ADC1_BASE + 0x0C0) as *mut u32;
+const ADC1_CALFACT: *const u32 = (ADC1_BASE + 0x0C4) as *const u32;
+
+const ADC_COMMON_BASE_ADDR: u32 = ADC1_BASE + 0x300;
+const ADC_CCR: *mut u32 = (ADC_COMMON_BASE_ADDR + 0x08) as *mut u32;
+
+const VREFBUF_ADDR: u32 = 0x5800_3C00;
+const VREFBUF_CSR_ADDR: *mut u32 = (VREFBUF_ADDR + 0x00) as *mut u32;
+
+pub unsafe fn configure_pwr() {
+    const VREFBUF_CSR_VRS: u32 = 0b111 << 4;
+    let mut vrefbuf_csr = read_volatile(VREFBUF_CSR_ADDR);
+    vrefbuf_csr &= !(VREFBUF_CSR_VRS);
+    write_volatile(VREFBUF_CSR_ADDR, vrefbuf_csr);
+
+    // while (read_volatile(VREFBUF_CSR_ADDR) & (1 << 3)) == 0 {}
+    const VREFBUF_CSR_HIZ_ENVR: u32 = 1 << 0;
+    let mut vrefbuf_csr = read_volatile(VREFBUF_CSR_ADDR);
+    vrefbuf_csr &= !(0b11 << 0);
+    vrefbuf_csr |= VREFBUF_CSR_HIZ_ENVR;
+    write_volatile(VREFBUF_CSR_ADDR, vrefbuf_csr);
+}
 
 
 pub unsafe fn rcc_enable_adc_clock() {
@@ -26,6 +44,19 @@ pub unsafe fn rcc_enable_adc_clock() {
     rcc_d3ccipr &= !(0b11 << 16);
     rcc_d3ccipr |= 1 << 17;
     write_volatile(RCC_D3CCIPR_ADDR, rcc_d3ccipr);
+
+    let mut adc_ccr = read_volatile(ADC_CCR);
+    adc_ccr &= !(0b11 << 16);
+    adc_ccr |= 1 << 17;
+    write_volatile(ADC_CCR, adc_ccr);
+}
+
+pub unsafe fn rcc_read_d3ccipr() -> u32 {
+    read_volatile(RCC_D3CCIPR_ADDR)
+}
+
+pub unsafe fn adc_read_ccr() -> u32 {
+    read_volatile(ADC_CCR)
 }
 
 pub unsafe fn adc_power_up() {
@@ -38,13 +69,39 @@ pub unsafe fn adc_power_up() {
     const ADC1_CR_ADVREGEN: u32 = 1 << 28;
     let adc1_cr = read_volatile(ADC1_CR_ADDR);
     write_volatile(ADC1_CR_ADDR,  adc1_cr | ADC1_CR_ADVREGEN);
-    delay(20_000); // Wait for regulator startup
+    delay(1_000_000);
+    while (read_volatile(ADC1_ISR_ADDR) & (1 << 12) == 0) {}
+}
+
+pub unsafe fn adc_read_calfact() -> u32 {
+    read_volatile(ADC1_CALFACT)
+}
+
+pub unsafe fn adc_set_diff() {
+    const ADC1_DIFSEL_16: u32 = 1 << 16;
+    let adc1_difsel = read_volatile(ADC1_DIFSEL);
+    write_volatile(ADC1_DIFSEL, adc1_difsel & !(ADC1_DIFSEL_16));
+}
+
+pub unsafe fn adc_read_diff() -> u32 {
+    read_volatile(ADC1_DIFSEL)
 }
 
 pub unsafe fn adc_calibrate() {
+    const ADC1_CR_ADDIS: u32 = 1 << 1;
+    let adc1_cr = read_volatile(ADC1_CR_ADDR);
+    write_volatile(ADC1_CR_ADDR, adc1_cr | ADC1_CR_ADDIS);
+
+    while (read_volatile(ADC1_CR_ADDR) & (1 << 1)) != 0 {}
+    while (read_volatile(ADC1_CR_ADDR) & (1 << 0)) != 0 {}
+
     const ADC1_CR_ADCALDIF: u32 = 1 << 30;
     let adc1_cr = read_volatile(ADC1_CR_ADDR);
     write_volatile(ADC1_CR_ADDR, adc1_cr & !(ADC1_CR_ADCALDIF));
+
+    const ADC1_CR_ADCALLIN: u32 = 1 << 16;
+    let adc1_cr = read_volatile(ADC1_CR_ADDR);
+    write_volatile(ADC1_CR_ADDR, adc1_cr & !(ADC1_CR_ADCALLIN));
     
     const ADC1_CR_ADCAL: u32 = 1 << 31;
     let adc1_cr = read_volatile(ADC1_CR_ADDR);
@@ -56,11 +113,15 @@ pub unsafe fn adc_calibrate() {
 
 pub unsafe fn adc_enable() {
     // Enable the ADC with ADVREGEN still enabled
-    write_volatile(ADC1_CR_ADDR, (1 << 0) | (1 << 28)); // ADEN = 1
+    const ADC1_CR_ADEN: u32 = 1 << 0;
+    let adc1_cr: u32 = read_volatile(ADC1_CR_ADDR);
+
+    write_volatile(ADC1_CR_ADDR,  adc1_cr | ADC1_CR_ADEN); // ADEN = 1
     // Wait for ADC to be ready (ADRDY flag in ISR)
-    while (read_volatile(ADC1_ISR_ADDR) & 1) == 0 {}
+    while ((read_volatile(ADC1_ISR_ADDR) & 1) == 0) {}
     // Clear the ADRDY flag by writing 1 to it
-    write_volatile(ADC1_ISR_ADDR, 1);
+    let adc1_isr = read_volatile(ADC1_ISR_ADDR);
+    write_volatile(ADC1_ISR_ADDR, adc1_isr | 1);
 }
 
 pub unsafe fn adc_configure_single_conversion(channel: u32) {
@@ -69,10 +130,18 @@ pub unsafe fn adc_configure_single_conversion(channel: u32) {
     write_volatile(ADC1_SQR1_ADDR, adc1_sqr1 | ADC1_SQR1_CHANNEL);
 }
 
+pub unsafe fn adc_read_sqr() -> u32 {
+    read_volatile(ADC1_SQR1_ADDR)
+}
+
 pub unsafe fn adc_preselect_channel(channel: u32) {
     const ADC1_PCSEL_16: u32 = 1 << 16;
     let adc1_pcsel = read_volatile(ADC1_PCSEL);
     write_volatile(ADC1_PCSEL, adc1_pcsel | ADC1_PCSEL_16);
+}
+
+pub unsafe fn adc_read_pcsel() -> u32 {
+    read_volatile(ADC1_PCSEL)
 }
 
 pub unsafe fn adc_start_conversion() {
@@ -87,7 +156,8 @@ pub unsafe fn adc_wait_for_conversion() {
 
 pub unsafe fn adc_read_value() -> u32 {
     let result = read_volatile(ADC1_DR_ADDR);
+    // let adc1_isr = read_volatile(ADC1_ISR_ADDR);
     // Clear the EOC flag by writing 1 to it
-    write_volatile(ADC1_ISR_ADDR, 1 << 2);
+    // write_volatile(ADC1_ISR_ADDR, adc1_isr | (1 << 2));
     result
 }
--- src/main.rs
+++ src/main.rs
@@ -7,10 +7,9 @@ mod uart;
 use cortex_m::asm::nop;
 use cortex_m_rt::entry;
 use panic_halt as _;
-use crate::adc::{adc_calibrate, adc_configure_single_conversion, adc_enable, adc_power_up, adc_preselect_channel, adc_read_value, adc_start_conversion, adc_wait_for_conversion, rcc_enable_adc_clock};
-use crate::gpio::{gpio_set_pin, gpio_set_pin_mode, gpio_toggle_pin, rcc_enable_gpio_clock, GpioPort, PinMode};
+use crate::adc::{adc_calibrate, adc_configure_single_conversion, adc_enable, adc_power_up, adc_preselect_channel, adc_read_calfact, adc_read_ccr, adc_read_diff, adc_read_pcsel, adc_read_sqr, adc_read_value, adc_set_diff, adc_start_conversion, adc_wait_for_conversion, configure_pwr, rcc_enable_adc_clock, rcc_read_d3ccipr};
 use stm32h7xx_hal as _;
-use crate::gpio::{gpio_set_alternate_function, gpio_set_pin_mode, gpio_toggle_pin, rcc_enable_gpio_clock, GpioPort, PinMode};
+use crate::gpio::{gpio_read_pin_mode, gpio_set_alternate_function, gpio_set_pin_mode, gpio_toggle_pin, rcc_enable_gpio_clock, GpioPort, PinMode};
 use crate::gpio::AlternateFunction::{AF6, AF7};
 use crate::timer::delay;
 use crate::uart::{rcc_enable_uart1_clock, rcc_enable_uart4_clock, Uart};
@@ -21,7 +20,8 @@ fn main() -> ! {
     const LED_PIN_PORT: GpioPort = GpioPort::E;
     const LED_PIN: u8 = 3;
     const ADC_PIN_PORT: GpioPort = GpioPort::A;
-    const ADC_PIN: u8 = 0;
+    const ADC_PIN_P: u8 = 0;
+    const ADC_PIN_N: u8 = 1;
     const ADC_CHANNEL: u32 = 16;
 
     const UART1_BASE_ADDR: u32 = 0x4001_1000;
@@ -29,13 +29,7 @@ fn main() -> ! {
     const UART1_TX_PIN: u8 = 9;
     const UART1_RX_PIN: u8 = 10;
 
-    // const UART4_BASE_ADDR: u32 = 0x4000_4C00;
-    // const UART4_GPIO_PORT: GpioPort = GpioPort::A;
-    // const UART4_RX_PIN: u8 = 11;
-    // const UART4_TX_PIN: u8 = 12;
-
     let uart1 = Uart::new(UART1_BASE_ADDR);
-    // let uart4 = Uart::new(UART4_BASE_ADDR);
 
 
     unsafe {
@@ -45,38 +39,39 @@ fn main() -> ! {
         rcc_enable_gpio_clock(ADC_PIN_PORT);
         rcc_enable_adc_clock();
         
-        gpio_set_pin_mode(ADC_PIN_PORT, ADC_PIN, PinMode::ANALOG);
-        
+        gpio_set_pin_mode(ADC_PIN_PORT, ADC_PIN_P, PinMode::ANALOG);
+        // gpio_set_pin_mode(ADC_PIN_PORT, ADC_PIN_N, PinMode::ANALOG);
+
+        configure_pwr();
         adc_power_up();
-        adc_calibrate();
-        adc_enable();
         adc_configure_single_conversion(ADC_CHANNEL);
         adc_preselect_channel(ADC_CHANNEL);
+        adc_set_diff();
+        adc_calibrate();
+        adc_enable();
 
-        rcc_enable_gpio_clock(UART1_GPIO_PORT);
         gpio_set_pin_mode(UART1_GPIO_PORT, UART1_RX_PIN, PinMode::ALTERNATE);
         gpio_set_pin_mode(UART1_GPIO_PORT, UART1_TX_PIN, PinMode::ALTERNATE);
-        // rcc_enable_gpio_clock(UART4_GPIO_PORT);
-        // gpio_set_pin_mode(UART4_GPIO_PORT, UART4_RX_PIN, PinMode::ALTERNATE);
-        // gpio_set_pin_mode(UART4_GPIO_PORT, UART4_TX_PIN, PinMode::ALTERNATE);
 
         gpio_set_alternate_function(UART1_GPIO_PORT, UART1_RX_PIN, AF7);
         gpio_set_alternate_function(UART1_GPIO_PORT, UART1_TX_PIN, AF7);
-        // gpio_set_alternate_function(UART4_GPIO_PORT, UART4_RX_PIN, AF6);
-        // gpio_set_alternate_function(UART4_GPIO_PORT, UART4_TX_PIN, AF6);
 
         rcc_enable_uart1_clock();
-        // rcc_enable_uart4_clock();
 
         uart1.init(115200);
-        // uart4.init(115200);
-
     }
 
     let mut counter: u32 = 0;
 
     loop {
         let adc_value: u32;
+        let calfact: u32;
+        let diffsel: u32;
+        let sqr: u32;
+        let pcsel: u32;
+        let moder: u32;
+        let d3ccipr: u32;
+        let ccr: u32;
         unsafe {
             gpio_toggle_pin(LED_PIN_PORT, LED_PIN);
             delay(1_000_000);
@@ -84,9 +79,20 @@ fn main() -> ! {
             adc_wait_for_conversion();
             gpio_toggle_pin(LED_PIN_PORT, LED_PIN);
             adc_value = adc_read_value();
+            calfact = adc_read_calfact();
+            diffsel = adc_read_diff();
+            sqr = adc_read_sqr();
+            pcsel = adc_read_pcsel();
+            moder = gpio_read_pin_mode(GpioPort::A);
+            d3ccipr = rcc_read_d3ccipr();
+            ccr = adc_read_ccr();
+            let bytes = adc_value.to_be_bytes();
+            for b in &bytes {
+                uart1.send_byte(*b);
+            }
         }
 
         nop();
-        delay(400_000);
+        delay(1_000_000);
     }
 }
