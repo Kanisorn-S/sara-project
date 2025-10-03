edb2fd3f2df008ac8b748aec862f068113349174
Kanisorn-S <karamahati@gmail.com>
2025-10-03 14:32:07 +0700

    feat: blinky

--- src/main.rs
+++ src/main.rs
@@ -1,70 +1,37 @@
-#![no_std] #![no_main]
-mod gpio;
-mod timer;
-mod adc;
-mod uart;
+#![deny(warnings)]
+#![no_main]
+#![no_std]
 
-use cortex_m::asm::nop;
 use cortex_m_rt::entry;
+use stm32h7xx_hal::{pac, prelude::*};
 use panic_halt as _;
-use stm32h7xx_hal as _;
-use crate::adc::{ADC1};
-use crate::gpio::{gpio_set_alternate_function, gpio_set_pin_mode, gpio_toggle_pin, rcc_enable_gpio_clock, GpioPort, PinMode};
-use crate::gpio::AlternateFunction::{AF7};
-use crate::timer::delay;
-use crate::uart::{rcc_enable_uart1_clock, Uart};
-
 
 #[entry]
 fn main() -> ! {
-    const LED_PIN_PORT: GpioPort = GpioPort::E;
-    const LED_PIN: u8 = 3;
+    let cp = cortex_m::Peripherals::take().unwrap();
+    let dp = pac::Peripherals::take().unwrap();
 
-    const UART1_BASE_ADDR: u32 = 0x4001_1000;
-    const UART1_GPIO_PORT: GpioPort = GpioPort::A;
-    const UART1_TX_PIN: u8 = 9;
-    const UART1_RX_PIN: u8 = 10;
+    // Constrain and Freeze power
+    let pwr = dp.PWR.constrain();
+    let pwrcfg = pwr.freeze();
 
-    let uart1 = Uart::new(UART1_BASE_ADDR);
-    
-    let adc1 = ADC1::new();
+    // Constrain and Freeze clock
+    let rcc = dp.RCC.constrain();
+    let ccdr = rcc.sys_ck(100.MHz()).freeze(pwrcfg, &dp.SYSCFG);
 
+    let gpioe = dp.GPIOE.split(ccdr.peripheral.GPIOE);
 
-    unsafe {
-        rcc_enable_gpio_clock(LED_PIN_PORT);
-        gpio_set_pin_mode(LED_PIN_PORT, LED_PIN, PinMode::OUTPUT);
+    let mut led = gpioe.pe3.into_push_pull_output();
 
-
-        rcc_enable_gpio_clock(UART1_GPIO_PORT);
-        gpio_set_pin_mode(UART1_GPIO_PORT, UART1_RX_PIN, PinMode::ALTERNATE);
-        gpio_set_pin_mode(UART1_GPIO_PORT, UART1_TX_PIN, PinMode::ALTERNATE);
-
-        gpio_set_alternate_function(UART1_GPIO_PORT, UART1_RX_PIN, AF7);
-        gpio_set_alternate_function(UART1_GPIO_PORT, UART1_TX_PIN, AF7);
-
-        rcc_enable_uart1_clock();
-
-        uart1.init(115200);
-        
-        adc1.init();
-    }
-
+    let mut delay = cp.SYST.delay(ccdr.clocks);
 
     loop {
-        let adc_value: u32;
-        unsafe {
-            gpio_toggle_pin(LED_PIN_PORT, LED_PIN);
-            delay(1_000_000);
-            adc1.start_conversion();
-            adc1.wait_for_conversion();
-            adc_value = adc1.read_value();
-            let bytes = adc_value.to_be_bytes();
-            for b in &bytes {
-                uart1.send_byte(*b);
-            }
-        }
+        led.set_high();
+        delay.delay_ms(500_u16);
 
-        nop();
-        delay(1_000_000);
+        led.set_low();
+        delay.delay_ms(500_u16);
     }
+
+
 }
